b5f870c1c1b3151ebd9ef15c1a4477644a172b2f
Cééesar Henrique <cesaarhvb@gmail.com>
2024-08-26 20:31:57 -0300

    chore: add input validation and error handling to Roman Numeral Converter form

--- script.js
+++ script.js
@@ -1,1 +1,51 @@
+const numberInput = document.getElementById("number");
+const convertBtn = document.getElementById("convert-btn");
+const output = document.getElementById("output");
 
+const parseInput = () => {
+  return parseInt(numberInput.value);
+};
+
+const showOutput = () => {
+  output.classList.remove("hide");
+};
+
+const addErrorClass = () => {
+  output.classList.add("error");
+};
+
+const removeErrorClass = () => {
+  output.classList.remove("error");
+};
+
+const checkInputIsValid = () => {
+  const parsedInput = parseInput();
+
+  if (isNaN(parsedInput)) {
+    output.textContent = "Please enter a valid number";
+    return false;
+  }
+
+  if (parsedInput < 1) {
+    output.textContent = "Please enter a number greater than or equal to 1";
+    return false;
+  }
+
+  if (parsedInput > 3999) {
+    output.textContent = "Please enter a number less than or equal to 3999";
+    return false;
+  }
+
+  return true;
+};
+
+const convertToRoman = (num) => {};
+
+convertBtn.addEventListener("click", () => {
+  showOutput();
+  removeErrorClass();
+  if (!checkInputIsValid()) {
+    addErrorClass();
+    return;
+  }
+});
